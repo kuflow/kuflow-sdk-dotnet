ec6c6618a07051d7e1d0268007555e7874590b71
Zebensui Méndez Corujo <zmencor@kuflow.com>
2023-05-10 10:43:20 +0100

    WIP

---
+++ README.md
@@ -0,0 +1,4 @@
+
+using vscode:
+
+https://www.youtube.com/watch?v=fuBi4d7k1-M
---
+++ pocdotnet/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, 7eben!");
+
+        System.Console.WriteLine("Bye");
+    }
+}
