b9c958368b44830a1e5dad3d471520b936af7418
Zebensui Méndez Corujo <zmencor@kuflow.com>
2023-05-10 14:40:01 +0100

    WIP

---
+++ kuflow-rest/TestClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Kuflow.Rest;
+
+namespace Kuflow;
+class TestClient
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, REST!");
+
+
+        // var client = new TaskClient("http://localhost:8080/apis/external");
+
+        System.Console.WriteLine("Bye");
+
+
+    }
+}
